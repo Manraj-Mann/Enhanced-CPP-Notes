3cd4d2e7147dbbc7b6c08ea27c910ddd47401c23
MANRAJ SINGH <81899682+Manraj-Mann@users.noreply.github.com>
2024-07-17 14:19:05 +0530

    Update 04_ConstantsAndOptimizations.cpp

--- Constants and Strings/04_ConstantsAndOptimizations.cpp
+++ Constants and Strings/04_ConstantsAndOptimizations.cpp
@@ -3,7 +3,7 @@
 
 /* Notes : 
 
-
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 Normal variables : 
 
 // Non-const variables:
@@ -33,7 +33,9 @@ const int i { getNumber() }; // getNumber() is not a constant expression
 why important to mark constants ?
 
 => Constant expressions are always eligible for compile-time evaluation, meaning they are more likely to be optimized at compile-time. This produces faster and smaller code.
-    
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 1. Const is used to mark variable as not modifiable constant
 
 const int x { 3 + 4 }; // constant expression 3 + 4 must be evaluated at compile-time
@@ -43,23 +45,38 @@ int y { 3 + 4 };       // constant expression 3 + 4 may be evaluated at compile-
 
 3. Optimization of non-constant expressions : 
 
-int x { 7 };            // x is non-const
-std::cout << x << '\n'; // x is a non-constant subexpression
-
-Even though x is non-const, a smart compiler might realize that x will always evaluate to 7 in this particular program, and under the as-if rule, optimize the program to this:
-
-int x { 7 };
-std::cout << 7 << '\n';
-
-When a variable is removed from a program, we say the variable has been optimized out (or optimized away).
+    int x { 7 };            // x is non-const
+    std::cout << x << '\n'; // x is a non-constant subexpression
+    
+    Even though x is non-const, a smart compiler might realize that x will always evaluate to 7 in this particular program, and under the as-if rule, optimize the program to this:
+    
+    int x { 7 };
+    std::cout << 7 << '\n';
+    
+    When a variable is removed from a program, we say the variable has been optimized out (or optimized away).
 
 4. Ranking variables by the likelihood of the compiler being able to optimize them:
 
     1.Compile-time constant variables (always eligible to be optimized)
     2.Runtime constant variables
     3.Non-const variables (likely optimized in simple cases only)
-    
 
+5. As if Rule : The rule says compiler can optimize however it likes as long as the observeable behaviour of code doesn't change.
+
+=> One kind of expression that can always be evaluated at compile time is called a “constant expression”
+
+6. A constant expression is an expression that contains only compile-time constants and operators/functions that support compile-time evaluation.
+
+7. A compile-time constant is a constant whose value must be known at compile time. This includes:
+    1. Literals (e.g. ‘5’, ‘1.2’)
+    2. Constexpr variables (we discuss these shortly in lesson 5.5 -- Constexpr variables)
+    3. Const integral variables with a constant expression initializer (e.g. const int x { 5 };). This is a historical exception -- in modern C++, constexpr variables are preferred.
+    4. Non-type template parameters (see 11.10 -- Non-type template parameters). Enumerators (see 13.2 -- Unscoped enumerations).
+
+8. The most common type of operators and functions that support compile-time evaluation include:
+
+    1. Arithmetic operators with operands that are compile-time constants (e.g. 1 + 2)
+    2. Constexpr and consteval functions (we’ll discuss these later in the chapter)
 
 */
 
